050eb24335d5114c3af9344711b07d9bb79c7abe
truncet <theonlyeminem@gmail.com>
2021-05-02 13:08:19 +0545

    orders added

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -43,22 +43,7 @@ class CartScreen extends StatelessWidget {
                     ),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Provider.of<Order>(context, listen: false).addOrder(
-                        cart.items.values.toList(),
-                        cart.totalAmount,
-                      );
-                      cart.clear();
-                    },
-                    child: Text(
-                      'PLACE ORDER',
-                    ),
-                    style: ButtonStyle(
-                      foregroundColor: MaterialStateProperty.all(
-                          Theme.of(context).primaryColor),
-                    ),
-                  )
+                  OrderButton(cart: cart),
                 ],
               ),
             ),
@@ -78,6 +63,51 @@ class CartScreen extends StatelessWidget {
             itemCount: cart.items.length,
           )),
         ],
+      ),
+    );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  _OrderButtonState createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: (widget.cart.totalAmount <= 0 || _isLoading)
+          ? null
+          : () async {
+              setState(() {
+                _isLoading = true;
+              });
+              await Provider.of<Order>(context, listen: false).addOrder(
+                widget.cart.items.values.toList(),
+                widget.cart.totalAmount,
+              );
+              setState(() {
+                _isLoading = false;
+              });
+              widget.cart.clear();
+            },
+      child: _isLoading
+          ? CircularProgressIndicator()
+          : Text(
+              'PLACE ORDER',
+            ),
+      style: ButtonStyle(
+        foregroundColor:
+            MaterialStateProperty.all(Theme.of(context).primaryColor),
       ),
     );
   }
--- lib/screens/orders.screen.dart
+++ lib/screens/orders.screen.dart
@@ -5,8 +5,29 @@ import '../widgets/app_drawer.dart';
 import '../providers/orders.dart';
 import '../widgets/order_item.dart' as ord;
 
-class OrderScreen extends StatelessWidget {
+class OrderScreen extends StatefulWidget {
   static const routeName = '/orderscreen';
+
+  @override
+  _OrderScreenState createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  bool _isLoading = false;
+  @override
+  void initState() {
+    setState(() {
+      _isLoading = true;
+    });
+    Future.delayed(Duration.zero).then((_) async {
+      await Provider.of<Order>(context, listen: false).fetchAndSetOrders();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Order>(context);
@@ -17,14 +38,16 @@ class OrderScreen extends StatelessWidget {
         ),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return ord.OrderItem(
-            orderData.orders[index],
-          );
-        },
-        itemCount: orderData.orders.length,
-      ),
+      body: _isLoading
+          ? CircularProgressIndicator()
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return ord.OrderItem(
+                  orderData.orders[index],
+                );
+              },
+              itemCount: orderData.orders.length,
+            ),
     );
   }
 }
